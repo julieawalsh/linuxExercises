bedf981381ce6d084e9b240ce9075be641c64117
Bahiravi <brajkumar@wisc.edu>
2025-03-04 08:22:03 -0600

    added school.sh

--- school.sh
+++ school.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+
+cat tax2.output| { sum=0, count=0 ; while read n; do sum=$(($sum+$n)); count=$(($count+1)); avg=$(($sum/$count)); done; echo "sum is \$$sum and average is \$$avg"; }
+
