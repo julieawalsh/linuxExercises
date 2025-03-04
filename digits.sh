bdead225239c4ec170adb75a8eb24ccdb460e66b
Bahiravi <brajkumar@wisc.edu>
2025-03-04 08:38:35 -0600

    added digits.sh

--- digits.sh
+++ digits.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 
+echo {1000..2000}>p
+pattern="(^[01]+$)"
+cat p | sed 's/ /\n/g' | { sum=0; while read n; do if [[ $n =~ $pattern ]] ; then sum=$(($sum+$n)); fi; done; echo "sum is $sum"; }
