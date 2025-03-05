9bb4ae9a4f2c874f5cbe0957bd6122db3c1b4731
Stella Ahn <ahn62@wisc.edu>
2025-03-05 12:15:17 -0600

    changed filename and added pipelines

--- school.sh
+++ school.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-cat tax2.output| { sum=0, count=0 ; while read n; do sum=$(($sum+$n)); count=$(($count+1)); avg=$(($sum/$count)); done; echo "sum is \$$sum and average is \$$avg"; }
+cat Property_Tax_Roll.csv | grep 'MADISON SCHOOLS' | cut -d ',' -f 7 | { sum=0;count=0; while read n; do sum=$(($sum + $n));count=$(($count+1)); avg=$(($sum/$count)); done; echo "sum is \$$sum and average is \$$avg"; }
 
